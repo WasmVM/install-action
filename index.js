591c3754bb3280188af928b0240dbc70ac0f963c
LuisHsu <luishsuplus@gmail.com>
2024-03-14 01:05:47 -0700

    Squashed commit of the following:
    commit b21da52c314457a3a5c5ecffd35b5ce07898889b
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Thu Mar 14 01:04:30 2024 -0700
    
        Test MacOS
    
    commit 989f7504737efdfd6c761ea44f8df0d8cadac356
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Thu Mar 14 01:01:34 2024 -0700
    
        dpkg install
    
    commit b0e650f413c6038cb89d8fdb90718030cbd37a49
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Thu Mar 14 01:00:08 2024 -0700
    
        Use wget
    
    commit 53aaf49fc9a2a2b7ba8ca5fbbda9db1c2cba78f9
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Thu Mar 14 00:57:41 2024 -0700
    
        Not extract deb file
    
    commit c0705da1618f7c0ca7497d689a01bed5aafc7e95
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Thu Mar 14 00:55:26 2024 -0700
    
        list files
    
    commit 960d77759a84227e5e0112c9ab4e4a3d45178e3e
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Thu Mar 14 00:49:21 2024 -0700
    
        Debug download
    
    commit 0c427e0909ac96db8e57bd494f2e730beab22d33
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Thu Mar 14 00:42:10 2024 -0700
    
        Use dpkg instead of apt
    
    commit 3824ecde68128877c311a908ed56fb16b5c495bb
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Thu Mar 14 00:40:26 2024 -0700
    
        Fix download
    
    commit 2f47950a0244cd7abd9d5322e73141ee6ff3675a
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Thu Mar 14 00:35:44 2024 -0700
    
        Install package
    
    commit 924c995c089b11c378d2f39d8b6b7f0c240b1e60
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Thu Mar 14 00:18:00 2024 -0700
    
        FIx readdir
    
    commit a05be308421246bfcfd53d72724c4754b8e7781c
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Thu Mar 14 00:16:59 2024 -0700
    
        Use https instead of http
    
    commit 6da7e458390c35f9fc4ac1fb92f4add0539195bb
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Thu Mar 14 00:16:12 2024 -0700
    
        Download package
    
    commit c75c375e27c56d72d35c48f31cffa70204f01f7a
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Wed Mar 13 23:16:34 2024 -0700
    
        Fix package regex
    
    commit 8872f6727633c3300d4cda9b9d929bccd159d028
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Wed Mar 13 23:07:57 2024 -0700
    
        Update index.js
    
    commit cc9deba4e5514550e7aa259225f9f7d74489275f
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Wed Mar 13 23:06:27 2024 -0700
    
        Filter asset
    
    commit 1a0849907eb958f20f399073e7babebc8d342b3e
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Wed Mar 13 22:54:27 2024 -0700
    
        Update index.js
    
    commit 7bbff5abc4e4a1bde9c7128acf3f092cab1e11e9
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Wed Mar 13 22:49:46 2024 -0700
    
        Update 'token' input in action
    
    commit 8c40db38b578519a6a5a1e085f0f0e0cc37225a4
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Wed Mar 13 22:41:39 2024 -0700
    
        Test action
    
    commit dfe7452ac517aa6f48c87c5c97b695c755bde221
    Author: LuisHsu <luishsuplus@gmail.com>
    Date:   Wed Mar 13 22:41:32 2024 -0700
    
        Update action.yml

--- index.js
+++ index.js
@@ -1,11 +1,71 @@
 const Core = require('@actions/core');
 const Github = require('@actions/github');
-const Path = require("path")
-const fs = require('fs')
+const Path = require("path");
+const fs = require('fs');
+const download = require('download');
+const child_process = require('child_process');
 
 try {
     // Get octokit
     const Octokit = Github.getOctokit(Core.getInput('token'));
+    // Get release
+    Promise.resolve(Core.getInput('version'))
+    .then(version => {
+        if(version){
+            return Octokit.rest.repos.getReleaseByTag({
+                owner: "WasmVM",
+                repo: "WasmVM",
+                tag: version
+            });
+        }else{
+            return Octokit.rest.repos.getLatestRelease({
+                owner: "WasmVM",
+                repo: "WasmVM"
+            });
+        }
+    })
+    // Get package link according to platform
+    .then(release => {
+        const dev_suffix = (Core.getInput('dev') == "true") ? "-dev" : "";
+        const platform_map = {
+            'linux' : new RegExp(`wasmvm${dev_suffix}_.*\.deb`),
+            'darwin': new RegExp(`WasmVM${dev_suffix}\.pkg`)
+        };
+        return release.data.assets.find(asset => asset.name.match(platform_map[process.platform]));
+    })
+    // Download package
+    .then(package => {
+        return new Promise(resolve => {
+            child_process.spawn(`wget ${package.browser_download_url}`, {shell: true})
+            .on('close', code => {
+                if(code){
+                    Core.error("Download failed")
+                }else{
+                    resolve(package.name);
+                }
+            })
+        })
+    })
+    .then(file_path => {
+        const command_map = {
+            'linux' : `sudo dpkg -i ${file_path}`,
+            'darwin': `sudo installer -pkg ${file_path} -target /`
+        };
+        return new Promise(resolve => {
+            child_process.spawn(command_map[process.platform], {stdio: [0, 1, 2], shell: true})
+            .on('close', code => {
+                if(code){
+                    Core.error("Install failed")
+                }else{
+                    resolve();
+                }
+            })
+        })
+    })
+    .then(() => {
+        console.log("WasmVM installed")
+    })
+
     // // Read & parse release note
     // let note_content = fs.readFileSync(Path.resolve(Core.getInput('note')), {encoding: 'utf8'});
     // const [tag_str, tag_name] = note_content.match(/^\`(.*)\`\n/);
