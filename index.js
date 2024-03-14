c0705da1618f7c0ca7497d689a01bed5aafc7e95
LuisHsu <luishsuplus@gmail.com>
2024-03-14 00:55:26 -0700

    list files

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ try {
     })
     .then(file_path => {
         const command_map = {
-            'linux' : `sudo dpkg -i ${file_path}`,
+            'linux' : `ls -l`,
             'darwin': `sudo installer -pkg ${file_path} -target /`
         };
         return new Promise(resolve => {
